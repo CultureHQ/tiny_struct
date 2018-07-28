a32decea91a1bc594d8b89c75a27f28f83ca2b2b
Kevin Deisz <kevin.deisz@gmail.com>
2018-07-28 18:28:40 -0400

    Bump dependencies

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 require 'simplecov'
 SimpleCov.start
 
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'tiny_struct'
 
 require 'minitest/autorun'
