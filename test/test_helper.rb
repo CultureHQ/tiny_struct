f56384de11a00e7ccf3ff3072212f8d5a9074131
Kevin Deisz <kevin.deisz@gmail.com>
2020-02-05 14:59:49 -0500

    Upgrade to new actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  enable_coverage :branch
+end
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'tiny_struct'
